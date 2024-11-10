f474464d9a94c96c95b460e3b308cff5ea6570d7
Mr. Gray <iamgraydiamond@gmail.com>
2024-11-10 23:14:44 +0100

    Config changes for testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    root: "./src",
     build: {
-        outDir: 'dist'
+        outDir: './dist',  // Output to the 'dist' folder in the root
+        rollupOptions: {
+            input: './src/my-profile.html'  // Explicitly set the entry point to index.html
+        }
     }
 });
